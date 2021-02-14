cdb4a20d8a90a83330ed9866416965662208884c
Lekan Koku <kokulekan23@gmail.com>
2021-02-14 12:54:51 +0100

    fix

--- src/components/Sorting/index.js
+++ src/components/Sorting/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import NavLink from "../NavLink";
-import { useRouteMatch } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,18 +12,18 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Home() {
   const classes = useStyles();
-  let url = 'sorting';
+  let subpath = '/sorting';
   return (
     <List
       component="nav"
       className={classes.root}
     >
         
-        <NavLink pathname="Bubble Sort" path={`${url}/bubble-sort`} />
-        <NavLink pathname="Selection Sort" path={`${url}/selection-sort`} />
-        <NavLink pathname="Quick Sort " path={`${url}/quick-sort`} />
-        <NavLink pathname="Insertion Sort" path={`${url}/insertion-sort`} />
-        <NavLink pathname="Merge Sort" path={`${url}/merge-sort`} />
+        <NavLink pathname="Bubble Sort" path={`${subpath}/bubble-sort`} />
+        <NavLink pathname="Selection Sort" path={`${subpath}/selection-sort`} />
+        <NavLink pathname="Quick Sort " path={`${subpath}/quick-sort`} />
+        <NavLink pathname="Insertion Sort" path={`${subpath}/insertion-sort`} />
+        <NavLink pathname="Merge Sort" path={`${subpath}/merge-sort`} />
 
            
               
